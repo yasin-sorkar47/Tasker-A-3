66ccb2f1cdecb39e509e1a045524b1ddfd95f1d1
yasin-sorkar47 <mdyacinsorkar17@gmail.com>
2024-01-27 23:32:01 +0600

    assignment_three

---
+++ src/components/Button.jsx
@@ -0,0 +1,13 @@
+function Button(props) {
+    return (
+        <button
+            type={props.type ? props.type : "button"}
+            className={`${props.className} rounded-md px-3.5 py-2.5 text-sm font-semibold`}
+            onClick={props.onClick}
+        >
+            {props.children}
+        </button>
+    );
+}
+
+export default Button;
---
+++ src/sections/Table.jsx
@@ -0,0 +1,128 @@
+import TableRow from "./TableRow";
+import Form from "./Form";
+import { useContext, useState } from "react";
+import { TaskContext } from "../context";
+import Modal from "./Modal";
+import { toast } from "react-toastify";
+import DeleteModal from "../components/DeleteModal";
+
+function Table() {
+    const { state, dispatch } = useContext(TaskContext);
+    const [modal, setModal] = useState(false);
+    const [editTask, setEditTask] = useState(null);
+    const [deleteModal, setDeleModal] = useState(false);
+    const [deleteItem, setDeleteItem] = useState({});
+
+    const editHandler = (task) => {
+        setEditTask(task);
+        setModal(true);
+    };
+
+    const deleteHandler = (task) => {
+        setDeleModal(true);
+        setDeleteItem(task);
+    };
+
+    const deleteItemAfterAgree = () => {
+        dispatch({
+            type: "DELETE_ITEM",
+            payload: {
+                ...deleteItem,
+            },
+        });
+        setDeleModal(false);
+        toast.success("Task removed successfully");
+    };
+
+    const favoriteHandler = (task) => {
+        dispatch({
+            type: "ADD_FAVORITE",
+            payload: {
+                ...task,
+            },
+        });
+    };
+
+    return (
+        <section className="mb-20" id="tasks">
+            <div className="container m-auto">
+                <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
+                    <div className="mb-14 items-center justify-between sm:flex">
+                        <h2 className="text-2xl font-semibold max-sm:mb-4">
+                            Your Tasks
+                        </h2>
+                        <Form />
+                    </div>
+                    <div className="overflow-auto">
+                        <table className="table-fixed overflow-auto xl:w-full">
+                            <thead>
+                                <tr>
+                                    <th className="p-4 pb-8 text-sm font-semibold capitalize w-[48px]"></th>
+                                    <th className="p-4 pb-8 text-sm font-semibold capitalize w-[300px]">
+                                        Title
+                                    </th>
+                                    <th className="p-4 pb-8 text-sm font-semibold capitalize w-full">
+                                        Description
+                                    </th>
+                                    <th className="p-4 pb-8 text-sm font-semibold capitalize md:w-[350px]">
+                                        Tags
+                                    </th>
+                                    <th className="p-4 pb-8 text-sm font-semibold capitalize md:w-[100px]">
+                                        Priority
+                                    </th>
+                                    <th className="p-4 pb-8 text-sm font-semibold capitalize md:w-[100px]">
+                                        Options
+                                    </th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {state.taskData.length === 0 ? (
+                                    <tr>
+                                        <td colSpan={6}>
+                                            <h4 className="bg-gray-800 py-4 text-xl text-center text-[#00D991A1]">
+                                                No Task Found
+                                            </h4>
+                                        </td>
+                                    </tr>
+                                ) : (
+                                    state.taskData.map((task) => {
+                                        {
+                                            if (task.status !== false) {
+                                                return (
+                                                    <TableRow
+                                                        onEdit={() =>
+                                                            editHandler(task)
+                                                        }
+                                                        onDelete={deleteHandler}
+                                                        onFavorite={
+                                                            favoriteHandler
+                                                        }
+                                                        key={task.id}
+                                                        task={task}
+                                                    />
+                                                );
+                                            }
+                                        }
+                                    })
+                                )}
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+            {modal && (
+                <Modal editTask={editTask} onClose={() => setModal(false)} />
+            )}
+
+            {deleteModal && (
+                <DeleteModal
+                    onAgree={deleteItemAfterAgree}
+                    onClose={() => setDeleModal(false)}
+                    title="Are you sure, you want to delete this item?"
+                />
+            )}
+        </section>
+    );
+}
+
+export default Table;
